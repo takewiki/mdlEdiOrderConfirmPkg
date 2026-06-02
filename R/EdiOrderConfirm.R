d9c40fe94605257b348fdd89cc6c87f6d46a8451
takewiki <hulilei@takeiwki.com.cn>
2026-06-02 09:31:09 +0000

    v1.0.1

--- R/EdiOrderConfirm.R
+++ R/EdiOrderConfirm.R
@@ -26,3 +26,31 @@ EdiOrderConfirm_update <- function(dms_token, FMessageNumber, FLineItemNumber, F
   return(res)
 
 }
+
+
+
+#' EDI销售订单查询
+#'
+#' @param FMessageNumber
+#' @param FLineItemNumber
+#' @param dms_token
+#'
+#' @return 两个数的和
+#' @export
+#'
+#' @examples
+#' EdiOrderConfirm_view()
+EdiOrderConfirm_view <- function(dms_token, FMessageNumber, FLineItemNumber) {
+
+
+  sql=paste0("
+             exec rds_proc_Edi_GetORDRSPSchedule '",FMessageNumber,"','",FLineItemNumber,"'
+             ")
+
+  res = tsda::sql_select2(token =dms_token ,sql = sql)
+
+
+
+  return(res)
+
+}
